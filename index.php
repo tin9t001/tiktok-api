e59d9af4c169df9e07e52eabed02eebbcc8bb65a
tin9t001 <117098370+tin9t001@users.noreply.github.com>
2022-10-31 22:05:49 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,38 +1,9 @@
 <?php
 if(isset($_GET['tiktok_url'])){
-   function CallAPI($method, $url, $data = false)
-    {
-        $curl = curl_init();
+    $content =     file_get_contents("https://tin9-tiktok-api.herokuapp.com/?tiktok_url=".$_GET['tiktok_url']);
+    
+    var_export(json_decode($content);
 
-        switch ($method)
-        {
-            case "POST":
-                curl_setopt($curl, CURLOPT_POST, 1);
-
-                if ($data)
-                    curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
-                break;
-            case "PUT":
-                curl_setopt($curl, CURLOPT_PUT, 1);
-                break;
-            default:
-                if ($data)
-                    $url = sprintf("%s?%s", $url, http_build_query($data));
-        }
-
-        // Optional Authentication:
-        curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
-        curl_setopt($curl, CURLOPT_USERPWD, "username:password");
-
-        curl_setopt($curl, CURLOPT_URL, $url);
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-
-        $result = curl_exec($curl);
-
-        curl_close($curl);
-
-        return $result;
-    }
-    CallAPI('GET', '$_GET['tiktok_url'])
+   
 }
 ?>
