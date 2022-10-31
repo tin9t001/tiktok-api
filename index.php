8f1ddd916228173f269b52b93930de0ba362963d
tin9t001 <117098370+tin9t001@users.noreply.github.com>
2022-10-31 22:11:52 +0700

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,12 @@
 if(isset($_GET['tiktok_url'])){
     $content =     file_get_contents("https://api.douyin.wtf/api?url=".$_GET['tiktok_url']);
     
-    $result  = json_decode($content);
-    return $result;
+    $action = "GET";
+    $url = $content;
+    echo "Trying to reach ...";
+    echo $url;
+    $parameters = array("param" => "value");
+    $result = CurlHelper::perform_http_request($action, $url, $parameters);
+    echo print_r($result)
 }
 ?>
