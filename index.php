edbe327a970c3913f9509016676c1d9e08812b74
tin9t001 <117098370+tin9t001@users.noreply.github.com>
2022-10-31 21:58:36 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -3,6 +3,6 @@ if(isset($_GET['tiktok_url'])){
     $content =     file_get_contents("https://api.douyin.wtf/api?url=".$_GET['tiktok_url']);
     
     $result  = json_decode($content);
-    var_dump($result);
+    return $result;
 }
 ?>
