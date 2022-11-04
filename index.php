ec9ae2dea8b0577e0368464a063132b8bab671a3
tin9t001 <117098370+tin9t001@users.noreply.github.com>
2022-11-04 22:01:11 +0700

    Update index.php

--- index.php
+++ index.php
@@ -4,13 +4,8 @@ if(isset($_GET['tiktok_url'])){
     // print_r($content);
 
     $yummy = json_decode($content);
-    // print_r($yummy->nwm_video_url);
-
-    // $res = array('nwm_video_url' => $yummy->nwm_video_url);
+    print_r($yummy->nwm_video_url);\
     
 }
 
 ?>
-<video width="320" autoplay controls>
-  <source src="<?=$yummy->nwm_video_url?>" type="video/mp4">
-</video>
